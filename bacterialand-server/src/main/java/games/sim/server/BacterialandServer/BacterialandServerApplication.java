907aa82d78e1a4520caa52e75840cefba1958d88
Ignacio <ignaciogomezsanchez@outlook.fr>
2020-04-25 08:20:12 +0200

    Init commit architecture settled

---
+++ bacterialand-server/src/main/java/games/sim/server/BacterialandServer/BacterialandServerApplication.java
@@ -0,0 +1,13 @@
+package games.sim.server.BacterialandServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacterialandServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacterialandServerApplication.class, args);
+	}
+
+}
---
+++ bacterialand-server/src/test/java/games/sim/server/BacterialandServer/BacterialandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package games.sim.server.BacterialandServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacterialandServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
