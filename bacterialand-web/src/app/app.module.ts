910fe5235f82722863bf94f1a8ad4e826a14f88f
Ignacio <ignaciogomezsanchez@outlook.fr>
2020-09-16 15:21:26 +0200

    Hexaboard start set up

--- bacterialand-web/src/app/app.module.ts
+++ bacterialand-web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+// import { HexaCell } from './cell/hexa-cell/hexa-cell.component';
+import { HexaBoardComponent } from './board/hexa-board/hexa-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    // HexaCell,
+    HexaBoardComponent
   ],
   imports: [
     BrowserModule,
